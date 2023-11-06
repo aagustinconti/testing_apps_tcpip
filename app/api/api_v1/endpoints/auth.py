3fc8c9d4756d8f9080670cddf11d2c8729626bc2
Agustín Principe <principeagustin@gmail.com>
2023-11-06 10:06:51 -0300

    fix(api): User endpoint and jwt

--- app/api/api_v1/endpoints/auth.py
+++ app/api/api_v1/endpoints/auth.py
@@ -4,8 +4,10 @@ from fastapi import APIRouter, Body, Depends
 from starlette.exceptions import HTTPException
 from starlette.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 
+from app.crud.shortcuts import check_free_username_and_email
+
 from ....core.config import ACCESS_TOKEN_EXPIRE_MINUTES
-from ....core.jwt import create_access_token
+from ....core.jwt import create_access_token, get_current_user_authorizer
 from ....crud.user import create_user, get_user_by_email
 from ....db.mongodb import AsyncIOMotorClient, get_database
 from ....models.user import User, UserInCreate, UserInLogin, UserInResponse
@@ -39,7 +41,7 @@ async def login(
 async def register(
         user: UserInCreate = Body(..., embed=True), db: AsyncIOMotorClient = Depends(get_database)
 ):
-    # await check_free_username_and_email(db, user.username, user.email)
+    await check_free_username_and_email(db, user.username, user.email)
 
     async with await db.start_session() as s:
         async with s.start_transaction():
