5c4c048f357c217af55808fe4ba48ced2289970b
Agustín Principe <principeagustin@gmail.com>
2023-12-09 15:17:00 -0300

    chore(ui): Change update product data format

--- ui/pages/api/product/update.ts
+++ ui/pages/api/product/update.ts
@@ -23,7 +23,7 @@ export default async function updateProduct(req: NextApiRequest, res: NextApiRes
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                new_product: {
+                product: {
                     product_code: code,
                     name: name,
                     price: price,
